8b8a0502a9d2db3c169476b236886f13cdea6c47
AnastasiiaOltika <anastasiiaoltika@Noutbuk-AnastasiiaOltika.local>
2025-07-24 20:03:52 +0200

    add fixtures

--- conftest.py
+++ conftest.py
@@ -7,7 +7,21 @@ def browser():
     options = Options()
     options.add_argument('--headless')
     browser = webdriver.Chrome(options=options)
+    # locally
+    # browser = webdriver.Chrome()
     browser.maximize_window()
     browser.implicitly_wait(3)
     yield browser
     browser.close()
+
+@pytest.fixture()
+def separator():
+    # print('=' + 10) for error
+    yield 'value'
+    print('test finished')
+
+@pytest.fixture(scope='session')
+def all_tests():
+    print('before all')
+    yield
+    print('After all')
