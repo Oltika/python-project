a70b36b8c7e661668163cc5b79ab4adc7f027a77
AnastasiiaOltika <anastasiiaoltika@Noutbuk-AnastasiiaOltika.local>
2025-07-24 18:58:15 +0200

    add temp solution for test

--- tests/test_site.py
+++ tests/test_site.py
@@ -1,5 +1,18 @@
 from pages.homepage import HomePage
 from pages.loginpage import LoginPage
+from selenium import webdriver
+import pytest
+from selenium.webdriver.chrome.options import Options
+
+# temp solution
+@pytest.fixture()
+def init_browser():
+    browser = webdriver.Chrome()
+    browser.maximize_window()
+    browser.implicitly_wait(3)
+    yield browser
+    browser.close()
+
 
 def test_open_s6(init_browser):
     home_page = HomePage(init_browser)
